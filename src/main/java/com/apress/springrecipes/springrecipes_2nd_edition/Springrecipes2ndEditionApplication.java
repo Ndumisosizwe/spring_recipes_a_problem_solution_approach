9eacf7b8d02e556d9688b340c8abf08c483e78d6
Ndumiso <ndumiso.mhlongo@supergrp.com>
2019-01-07 12:04:13 +0200

    init commit after mvn install

---
+++ src/main/java/com/apress/springrecipes/springrecipes_2nd_edition/Springrecipes2ndEditionApplication.java
@@ -0,0 +1,14 @@
+package com.apress.springrecipes.springrecipes_2nd_edition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrecipes2ndEditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrecipes2ndEditionApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/apress/springrecipes/springrecipes_2nd_edition/Springrecipes2ndEditionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.apress.springrecipes.springrecipes_2nd_edition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springrecipes2ndEditionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
